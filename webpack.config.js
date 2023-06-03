24a7c3e0c224065add2a55b5ca7eb0b03ea5c6d4
Zakaria ElQotbi <zakaria@elqotbi.com>
2023-06-03 22:57:15 +0200

    fixup package.json

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 module.exports = {
     entry: './src/LeafletMapPlugin',
     output: {
-        path: path.resolve(__dirname, "dist"),
         filename: "openmct-leaflet.js",
+        path: path.resolve(__dirname, "dist"),
         library: "LeafletMapPlugin",
         libraryTarget: "umd"
     },
